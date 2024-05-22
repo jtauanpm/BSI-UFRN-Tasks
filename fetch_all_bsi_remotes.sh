69e6f7854d9d29464233ee8a97111c32f4605116
jtauanpm <jtauan10@gmail.com>
2024-05-22 15:33:07 -0300

    fix

---
+++ fetch_all_bsi_remotes.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Lista de diretórios
+DIRECTORIES=(
+    "Assembly-MIPS"
+    "aws-dynamodb-demo"
+    "flutter_movies_app"
+    "lol-champions"
+    "POO-I"
+    "prog-web"
+    "projeto-admin-bd"
+    "sdtp-redes"
+    "SIG-Finance"
+)
+
+# Loop para cada diretório
+for dir in "${DIRECTORIES[@]}"; do
+    # Navegar até o diretório
+    cd "$dir"
+    
+    # Excluir o diretório .git
+    rm -rf .git
+    
+    # Voltar para o diretório anterior
+    cd ..
+done
